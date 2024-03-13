ff8682ac2f54d9633b8867142fab77ea461ca8ea
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-13 17:36:46 -0300

    Data types.

---
+++ data_types.go
@@ -0,0 +1,15 @@
+package main
+
+import("fmt")
+
+func main() {
+	var a bool = true
+	var b int = 5
+	var c float32 = 3.14
+	var d string = "Hi"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
