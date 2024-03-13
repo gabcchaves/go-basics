4c51e0801ce938302ef29b20b4b687b3f84f31d2
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-13 17:25:44 -0300

    Variables.

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import("fmt")
+
+func main() {
+	var student1 string = "Someone"
+	var student2 = "Anotherone"
+	fmt.Println(student1)
+	fmt.Println(student2)
+
+	var novalue string
+	fmt.Println(novalue)
+}
