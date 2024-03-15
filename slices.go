02e43aeffe1860a296a6ca133aa2a78fb6f7f1ed
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 08:54:03 -0300

    Append elements to slice.

--- slices.go
+++ slices.go
@@ -2,6 +2,7 @@ package main
 import("fmt")
 
 func main() {
-	slice := make([]int, 10, 100)
+	slice := make([]int, 5, 10)
+	slice = append(slice, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 	fmt.Println(slice)
 }
