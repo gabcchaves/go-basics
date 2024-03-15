afa882a3c202e8315d2b957e80bd655ef62ef401
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 11:10:51 -0300

    Range keyword.

--- loops.go
+++ loops.go
@@ -2,9 +2,8 @@ package main
 import("fmt")
 
 func main() {
-	for i:=0; i < 5; i++ {
-		for j:=0; j < 5; j++ {
-			fmt.Println(i*j)
-		}
+	fruits := [3]string{"apple", "orange", "banana"}
+	for idx, val := range fruits {
+		fmt.Printf("%v\t%v\n", idx, val)
 	}
 }
