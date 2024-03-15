96fbfea4d0c38389f1c82b7ccb766f1404b0f5b4
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 08:57:58 -0300

    Change the length of a slice.

--- slices.go
+++ slices.go
@@ -3,8 +3,10 @@ import("fmt")
 
 func main() {
 	slice0 := []int{1, 2, 3}
-	slice1 := []int{4, 5, 6}
-	slice2 := append(slice0, slice1...)
 	
-	fmt.Println(slice2)
+	fmt.Println(slice0)
+
+	slice0 = slice0[0:2]
+
+	fmt.Println(slice0)
 }
