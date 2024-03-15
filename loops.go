4ef146d1fe9f91a35195e24a8ffc18fb50630af9
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 11:06:57 -0300

    For loop.

--- loops.go
+++ loops.go
@@ -2,7 +2,16 @@ package main
 import("fmt")
 
 func main() {
+	fmt.Println("HI");
 	for i:=0; i < 5; i++ {
 		fmt.Println(i)
+	}
+
+	for i:=5; i > 0; i-- {
+		fmt.Println(i)
+	}
+
+	for i:=0; i < 5; i++ {
+		fmt.Println("For loop");
 	}
 }
