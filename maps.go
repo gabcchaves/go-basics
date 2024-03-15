a74e05813a83bbaf2b97a15883f95106a8b9a206
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 11:27:10 -0300

    Access map elements.

--- maps.go
+++ maps.go
@@ -2,6 +2,8 @@ package main
 import("fmt")
 
 func main() {
-	var a map[int]string
-	fmt.Println(a)
+	var a = map[int]string{0:"apple", 1:"orange", 2:"banana"}
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+	fmt.Println(a[2])
 }
