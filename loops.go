acfc17ff0bdcea05b7309a5b19a15619d091f5c9
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-15 11:07:41 -0300

    Continue statement.

--- loops.go
+++ loops.go
@@ -2,16 +2,10 @@ package main
 import("fmt")
 
 func main() {
-	fmt.Println("HI");
 	for i:=0; i < 5; i++ {
+		if i == 3 {
+			continue
+		}
 		fmt.Println(i)
-	}
-
-	for i:=5; i > 0; i-- {
-		fmt.Println(i)
-	}
-
-	for i:=0; i < 5; i++ {
-		fmt.Println("For loop");
 	}
 }
