2faafe11e4bd58144464feb6c5b3b3a42fb139b4
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-21 17:32:24 -0300

    Exercise 1.

---
+++ variables_and_datatypes/e1.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	var a int = 5
+	var b float64 = 3.14
+	var c string = "Olá"
+	var d bool = true
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
