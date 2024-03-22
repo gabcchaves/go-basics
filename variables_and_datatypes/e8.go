1ee88de058e63468d1f3a9b3f8956eb7cd4bc9a4
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-22 17:37:09 -0300

    User var, const and walrus operator to assing values to variables.

---
+++ variables_and_datatypes/e8.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+    var v string = "Olá, pessoa!"
+    const c float64 = 3.14
+    w := "Walrus"
+
+    fmt.Println(v)
+    fmt.Println(c)
+    fmt.Println(w)
+}
