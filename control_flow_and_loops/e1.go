78780c3deecb2b4744914a80c5f1285306f3bc17
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-03-22 22:08:29 -0300

    Test if else statement.

---
+++ control_flow_and_loops/e1.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+    var a int = 1
+    var b int = 2
+
+    if a < b {
+        fmt.Println("a é menor que b.")
+    } else {
+        fmt.Println("b é maior que a.")
+    }
+}
